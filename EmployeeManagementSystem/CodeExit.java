8b92e44e1147d784c11b5e15f30d28ca3ee02d85
Mohamed Ibrahim <mohamed_im123@hotmail.com>
2023-09-27 16:19:21 +0100

    Initial Commit

---
+++ EmployeeManagementSystem/CodeExit.java
@@ -0,0 +1,11 @@
+/************************ To Exit from the EMS Portal *********************/
+
+class CodeExit
+{
+    public void out()
+    {
+        System.out.println("\n*****************************************");
+        System.out.println("*****************************************");
+        System.exit(0);
+    }
+}
---
+++ EmployeeManagementSystem/Employee_Show.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
+/************************ To Show details of Employee *********************/
+
+class Employee_Show
+{
+    public void viewFile(String s) throws Exception
+    {
+        File file = new File("file"+s+".txt");
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine())
+        {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
